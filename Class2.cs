dbc1f39774e295b6db588a0c1bfb3709911745cd
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-22 13:40:10 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class TDSdata
+    {
+        private string id;
+        private string currentdata;
+        private string counterpartyid;
+
+        public string getid()
+        {
+            return this.id;
+        }
+        public string getcurrentdata()
+        {
+            return this.currentdata;
+        }
+
+        public string getcounterpartyid()
+        {
+            return this.counterpartyid;
+        }
+
+        public void setid(string value)
+        {
+            this.id = value;
+        }
+        public void setcurrentdata(string value)
+        {
+            this.currentdata = value;
+        }
+
+        public void setcounterpartyid(string value)
+        {
+            this.counterpartyid = value;
+        }
+
+
+    }
+
+    class RDSDataModel
+    {
+        private string CounterPartyId;
+        private string Name;
+
+    }
+
+
+
+    class RiskCalculator
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("calculating");
+        }
+    }
+
+    class RiskInputDataMerger
+    {
+        public void Merge()
+
+        {
+            Console.WriteLine("merge data");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TDSdata d = new TDSdata();
+            d.setid("12");
+            d.setcurrentdata("65");
+            d.setcounterpartyid("75");
+
+            RiskCalculator e = new RiskCalculator();
+            e.Calculate();
+
+            RiskInputDataMerger s = new RiskInputDataMerger();
+            s.Merge();
+
+
+
+        }
+}
+}
