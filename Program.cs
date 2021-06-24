4ecefa529ea3b5692b3ab327aec39c738813c0ba
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-24 17:21:13 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace demo1
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TDSdata d = new TDSdata();
-            d.setid("12");
-            d.setcurrentdata("65");
-            d.setcounterpartyid("75");
+ 
 
-            RiskCalculator e = new RiskCalculator();
-            e.Calculate();
-
-            RiskInputDataMerger s = new RiskInputDataMerger();
-            s.Merge();
-
+            calculate instance = new calculate(50,5.1,40);
+            
 
 
         }
-}
+    }
 }
---
+++ calculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class calculate
+    {
+        
+        calculate(int weight , double height , int BMI )
+        {
+            
+
+            if(BMI<18.5)
+            {
+                Console.WriteLine("underweight");
+
+            }
+            else if (BMI >= 25)
+            {
+                Console.WriteLine("healthy");
+            }
+
+        }
+    }
+}
