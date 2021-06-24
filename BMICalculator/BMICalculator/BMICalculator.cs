2a353c0c627b7dcb60db002c33c23a9a827742f3
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-24 20:24:58 +0530

    Add files via upload

---
+++ BMICalculator/BMICalculator/BMICalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class BMICalculator
+    {
+        
+       
+            public float CalculateBMI(float height, float Weight)
+            {
+                float BMI = Weight / height;
+                return BMI;
+            }
+
+            public string CheckStatus(float bMI)
+            {
+                if (bMI < 18.5)
+                    return "underweight";
+                else if (bMI >= 25)
+                    return "overweight";
+                else
+                    return "healthy";
+            }
+        }
+    }
+
+
---
+++ BMICalculator/BMICalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMICalculator bMICalculator = new BMICalculator();
+            Console.WriteLine("Enter height in meters");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the weight in KG");
+            float weight = float.Parse(Console.ReadLine());
+            float BMI = bMICalculator.CalculateBMI(height, weight);
+            string status = bMICalculator.CheckStatus(BMI);
+            Console.WriteLine(status);
+
+        }
+    }
+}
