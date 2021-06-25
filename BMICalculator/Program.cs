12b3876bc8cf8dcbcc0dee4d25ed88991ffe5239
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-25 23:58:32 +0530

    updated

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            Display input = new Display();
+            float showMessage = input.UserInput();
+
+            ValidateUserInput validateStatus = new ValidateUserInput();
+            String display = validateStatus.DisplayMessage(showMessage);
+            Console.WriteLine("Check Status " + display);
+        }
+    }
+}
+
