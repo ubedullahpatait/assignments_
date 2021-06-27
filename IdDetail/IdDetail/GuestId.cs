902eadd259414be9a527c2337bd91978a5ddb124
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-27 11:50:31 +0530

    Update GuestId.cs

--- IdDetail/IdDetail/GuestId.cs
+++ IdDetail/IdDetail/GuestId.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IdDetail
+namespace IdCardDetail
 {
-    class GuestId:CardDetail
+   public class GuestIdCard:CardDetail
     {
         String _purpose;
-       public  GuestId(String name, String address,String dob ,long phoneNumber,String purpose):base(name, address,dob, phoneNumber)
+       public  GuestIdCard(String name, String address,String dob ,long phoneNumber,String purpose):base(name, address,dob, phoneNumber)
         {
             this._purpose = purpose;
         }
