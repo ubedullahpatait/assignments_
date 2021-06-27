8281cb0929ced8b1c734bb05e7015092c6ae7824
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-27 11:59:57 +0530

    Update Program.cs

--- IdDetail/IdDetail/Program.cs
+++ IdDetail/IdDetail/Program.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IdDetail
+namespace IdCardDetail
 {
     class Program
     {
         static void Main(string[] args)
         {
-            StudentId _student = new StudentId("ubed","shankar nagar","01/09/1996",8754692154,"1GI21MCA01","MCA","A");
+            StudentIdCard _student = new StudentIdCard("ubed","shankar nagar","01/09/1996",8754692154,"1GI21MCA01","MCA","A");
 
-            TeacherId _teacher = new TeacherId("kiran","vaibhav nagar","09/07/1985",986542158,"mechanical","Assistance Professor" ,"MECH987");
+            TeacherIdCard _teacher = new TeacherIdCard("kiran","vaibhav nagar","09/07/1985",986542158,"mechanical","Assistance Professor" ,"MECH987");
 
-            GuestId _guest = new GuestId("anand", "shanti nagar", "05/04/1980", 865321547, "Enquiry");
+            GuestIdCard _guest = new GuestIdCard("anand", "shanti nagar", "05/04/1980", 865321547, "Enquiry");
         }
     }
 }
