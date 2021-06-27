5c019ea08ea6e2b16b5feaf0eeae12c3d6166dfe
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-27 12:16:16 +0530

    Add files via upload

---
+++ IdCardDetail/IdCardDetail/CardDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdCardDetail
+{
+   public class CardDetail
+    {
+        private String _name, _address, _dob;
+        private long _phoneNumber;
+
+
+        public CardDetail(String name)
+        {
+            this._name = name;
+        }
+
+        public CardDetail(String name, String address, string dob) : this(name)
+        {
+
+            this._address = address;
+            this._dob = dob;
+
+        }
+        public CardDetail(String name, String address, string dob, long phoneNumber) : this(name, address, dob)
+        {
+
+            this._phoneNumber = phoneNumber;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ IdCardDetail/IdCardDetail/GuestIdCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdCardDetail
+{
+    public class GuestIdCard : CardDetail
+    {
+        String _purpose;
+        public GuestIdCard(String name, String address, String dob, long phoneNumber, String purpose) : base(name, address, dob, phoneNumber)
+        {
+            this._purpose = purpose;
+        }
+
+    }
+}
